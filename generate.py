f72a6c1909bbadb68d1aa168997930e4e008a7d4
dP <dp@dpointer.org>
2022-06-05 17:15:28 +0300

    Use grf-py instead of generating nml

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 from PIL import Image, ImageDraw
 
-import nml
+import grf
 
 SPRITE_SIZE = (20, 45)
 NUMBERS_SHEET = Image.open('numbers.png')
@@ -10,17 +10,15 @@ NUMBERS = [NUMBERS_SHEET.crop((i * 11, 0, i * 11 + 11, 11)) for i in range(20)]
 NUMBERS_MASK = [Image.eval(img, (lambda a: 255 if a == 1 else 1)).convert('1') for img in NUMBERS]
 
 
-class TreeSprite(nml.BaseSprite):
-    def __init__(self, sprite_id, index, stage, climate):
+class TreeSprite(grf.GraphicsSprite):
+    def __init__(self, index, stage, climate):
+        w, h = SPRITE_SIZE
+        super().__init__(w, h, xofs=-w // 2, yofs=-h)
         self.index = index
         self.stage = stage
         self.climate = climate
-        super().__init__(sprite_id, *SPRITE_SIZE)
-        self.ofs_x = -self.w // 2
-        self.ofs_y = -self.h
 
-    def draw(self, img):
-        x, y, w, h = self.x, self.y, self.w, self.h
+    def get_image(self):
         green_colors = [0x1c, 0x24, 0x57, 0xd0, 0x5c, 0x1d, 0x63, 0xd0]  # min 8 (arctic)
         blue_colors = [0x84, 0x8d, 0x97, 0x9e, 0xac, 0xcd, 0xd6, 0xa1]  # min 8 (arctic-snow)
         misc_colors = [0xb6, 0xbe, 0x39, 0x41, 0x3f, 0x4d, 0x77, 0xa5, 0xad, 0x15]  # min 10 (tropic)
@@ -33,51 +31,40 @@ class TreeSprite(nml.BaseSprite):
         }[self.climate][self.index]
         # color = {"arctic": 0x8C, "arctic-snow": 0x98, "temperate": 0x57, "tropic": 0x43, "toyland": 0xA5}[self.climate]
         s = 12
-        px = (w - s) // 2
+        px = (self.w - s) // 2
+        img = Image.new('P', (self.w, self.h))
+        img.putpalette(grf.PALETTE)
         imd = ImageDraw.Draw(img)
-        imd.rectangle((x, y, x + w, y + h), fill=0)
-        imd.rectangle((x + px, y + h - s - 1, x + w - px, y + h - 1), fill=color, outline=1)
+        imd.rectangle((px, self.h - s - 1, self.w - px, self.h - 1), fill=color, outline=1)
         for i in range(self.stage - 1):
-            imd.rectangle((x + px + 1 + i, y + h - s - i * 3 - 4, x + w - px - 1 - i, y + h - s - 1 - i * 3), fill=color, outline=1)
-        img.paste(NUMBERS[self.index], (x + px + 1, y + h - s), mask=NUMBERS_MASK[self.index])
-        # img.paste(NUMBERS[self.index], (x + px + 1, y + h - s))
-
-    def get_nml(self):
-        return (f'replace ({self.sprite_id}, "{self.file}") {{ [{self.x}, {self.y}, {self.w}, {self.h}, {self.ofs_x}, {self.ofs_y}] }}'
-                f'  // {self.climate} #{self.index} stage:{self.stage}')
+            imd.rectangle((px + 1 + i, self.h - s - i * 3 - 4, self.w - px - 1 - i, self.h - s - 1 - i * 3), fill=color, outline=1)
+        img.paste(NUMBERS[self.index], (px + 1, self.h - s), mask=NUMBERS_MASK[self.index])
+        return img, 8
 
 
-base_path = Path(__file__).parent.absolute()
+g = grf.NewGRF(
+    grfid=b'CMDT',
+    name='Debug Trees',
+    description='Trees for debugging',
+    version=2,
+    min_compatible_version=1,
+    url='https://github.com/citymania-org/debug-trees-grf',
+)
 
-build_dir = Path("build")
-build_dir.mkdir(parents=True, exist_ok=True)
-os.chdir(build_dir)
-
-trees = [TreeSprite(1576 + x, x // 7, x % 7 + 1, "temperate") for x in range(7 * 19)]
-trees += [TreeSprite(1709 + x, x // 7, x % 7 + 1, "arctic") for x in range(7 * 8)]
-trees += [TreeSprite(1765 + x, x // 7, x % 7 + 1, "arctic-snow") for x in range(7 * 8)]
-trees += [TreeSprite(1821 + x, x // 7, x % 7 + 1, "tropic") for x in range(7 * 18)]
-trees += [TreeSprite(1947 + x, x // 7, x % 7 + 1, "toyland") for x in range(7 * 9)]
-trees_ss = nml.SpriteSheet(trees)
-trees_ss.make_image("debug-trees.png", columns=7)
-
-lang_dir = Path("lang")
-lang_dir.mkdir(exist_ok=True)
-with open(lang_dir / "english.lng", "w") as f:
-    f.write("##grflangid 0x01\n")
-    f.write("STR_GRF_NAME :Debug Trees\n")
-    f.write("STR_GRF_DESCRIPTION :Trees for debugging\n")
-
-with open('debug-trees.nml', 'w') as f:
-    f.write('''\
-grf {
-    grfid: "CMDT";
-    name: string(STR_GRF_NAME);
-    desc: string(STR_GRF_DESCRIPTION);
-    version: 2;
-    min_compatible_version: 1;
+RANGES = {
+    "temperate": (1576, 19),
+    "arctic": (1709, 8),
+    "arctic-snow": (1765, 8),
+    "tropic": (1821, 18),
+    "toyland": (1947, 9),
 }
 
-''')
+g.add(grf.ReplaceOldSprites([(first, count * 7) for first, count in RANGES.values()]))
+for climate, (_, amount) in RANGES.items():
+    for x in range(7 * amount):
+        g.add(TreeSprite(x // 7, x % 7 + 1, climate))
 
-    trees_ss.write_nml(f)
+base_path = Path(__file__).parent.absolute()
+build_dir = base_path / 'build'
+build_dir.mkdir(exist_ok=True)
+g.write(build_dir / 'debug-trees.grf')
